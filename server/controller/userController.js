b20c746c48519731718c754c968bc205ce44f344
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2025-04-06 15:18:52 +0530

    Update method working properly

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -45,3 +45,19 @@ export const getSingleUser = async(req,res)=>{
         res.status(500).json({error:error})
     }
 }
+
+export const update = async(req,res)=>{
+    try {
+        const id = req.params.id;
+        const userExist = await User.findById(id);
+        if(!userExist){
+            return res.status(401).json({msg:"User not found"});
+        }
+        const updatedData = await User.findByIdAndUpdate(id, req.body,
+            {new:true});
+            res.status(200).json(updatedData);
+        
+    } catch (error) {
+        res.status(500).json({error:error});
+    }
+}
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { create, getAll, getSingleUser } from '../controller/userController.js';
+import { create, getAll, getSingleUser, update } from '../controller/userController.js';
 
 const route = express.Router();
 
 route.post("/create",create)
 route.get("/getall",getAll)
 route.get("/getSingleUser/:id",getSingleUser)
+route.put("/update/:id",update)
+
 
 export default route;
